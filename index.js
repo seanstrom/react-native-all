effc3859c2bbbed8131f4685eab9d03c68baa4ff
Sean Hagstrom <sean@seanhagstrom.com>
2024-05-16 13:08:03 +0100

    wip

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import { NativeModules } from 'react-native';
+import app from './app/index.js'
+
+if (__DEV__) {
+  NativeModules.DevSettings.setHotLoadingEnabled(false);
+}
+
+// AppRegistry.registerComponent(appName, () => App);
